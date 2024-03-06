e6a55b9b5a6f8722612288e60c7d4318f1b94d99
zpub <lil.rhymes9@gmail.com>
2024-03-06 20:52:57 +0300

    Initial commit

---
+++ src/main/java/com/example/mebelshik/Entities/Order.java
@@ -0,0 +1,2 @@
+package com.example.mebelshik.Entities;public class Order {
+}
---
+++ src/test/java/com/example/mebelshik/MebelshikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mebelshik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MebelshikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
